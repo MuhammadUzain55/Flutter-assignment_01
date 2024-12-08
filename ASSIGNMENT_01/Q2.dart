347f0509a885ec3cb6f08d2f3c52014a0dd6a2b9
MuhammadUzain55 <uzainsheikh55@gmail.com>
2024-12-08 19:58:25 +0500

    Add Q.2

---
+++ ASSIGNMENT_01/Q2.dart
@@ -0,0 +1,12 @@
+void main() {
+  int age1 = 32;
+  int age2 = 20;
+
+  if (age1 > age2) {
+    print("The age1 is the oldest person than age2");
+  } else if (age1 < age2) {
+    print("The age2 is the youngest person than age1");
+  } else {
+    print("Both are same age");
+  }
+}
