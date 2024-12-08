1dfe24247d9de802bc1864a7966956cb90ca5b4e
MuhammadUzain55 <uzainsheikh55@gmail.com>
2024-12-08 21:40:58 +0500

    assignment Q1

---
+++ ASSIGNMENT_01/Q1.dart
@@ -0,0 +1,13 @@
+void main() {
+ // Question #01
+
+  int length = 56;
+  int breadth = 69;
+
+  if (length == breadth) {
+    print("It is a square");
+  } else {
+    print("It is a rectangle");
+  }
+
+}
