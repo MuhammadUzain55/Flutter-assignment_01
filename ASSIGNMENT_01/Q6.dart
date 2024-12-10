81ec8646012a6bdffaa254eace87f6f1ea5012f3
MuhammadUzain55 <uzainsheikh55@gmail.com>
2024-12-10 13:07:02 +0500

    assignment Q6

---
+++ ASSIGNMENT_01/Q6.dart
@@ -0,0 +1,13 @@
+void main() {
+  String alphabet = "a";
+
+  if (alphabet == "a" ||
+      alphabet == "e" ||
+      alphabet == "i" ||
+      alphabet == "o" ||
+      alphabet == "u") {
+    print("it's a vowel");
+  } else {
+    print("it's a consonent");
+  }
+}
